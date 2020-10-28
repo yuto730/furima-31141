285a40be9c629508154be1f2e33a7ff5612de279
yuto730 <my30.design@gmail.com>
2020-10-28 20:32:27 +0900

    ユーザー管理機能の実装_修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,15 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  validates :nickname, presence: true
-  validates :email, uniqueness: true
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birthday
+  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
 end
