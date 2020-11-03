1e767be9bea2dc370a6cf3a41c5bd85ecb8c6fcf
yuto730 <my30.design@gmail.com>
2020-11-03 14:50:37 +0900

    商品削除機能_修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
