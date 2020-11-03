b12504a4ad5405d8e8f92a2419ba32f400a79d79
yuto730 <my30.design@gmail.com>
2020-11-03 12:20:27 +0900

    商品情報編集機能_修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path(item.id), method: :patch
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
